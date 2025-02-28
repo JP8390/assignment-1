264cd45305533f1a17751aa478f35f2aa24b92e6
PatilJatin <jatinp8390@gmail.com>
2025-02-28 21:18:18 +0530

    feat: update the navbar title.

--- src/component/layout/Navbar.tsx
+++ src/component/layout/Navbar.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="bg-blue-600 text-center py-5 ">
-      NextJS Pokemon Search App
-    </nav>
+    <nav className="bg-blue-600 text-center py-5 ">Pokemon Search App</nav>
   );
 };
 

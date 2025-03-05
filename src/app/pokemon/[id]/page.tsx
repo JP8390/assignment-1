54e3d8268a719267f5dbc3fbdb738dec46fe9429
PatilJatin <jatinp8390@gmail.com>
2025-03-05 18:09:48 +0530

    pokemon with dyanmic metadata added.

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,24 +1,55 @@
 import { notFound } from "next/navigation";
 import { fetchPokemonById } from "@/utils/fetchPokemon";
 import PokemonDetails from "@/component/pages/PokemonDetails";
+import { Metadata } from "next";
 
 export const revalidate = 86400;
 
-export async function generateStaticParams() {
-  const ids = Array.from({ length: 10 }, (_, i) => ({
-    id: (i + 1).toString(),
-  }));
-  return ids;
+type Params = Promise<{ id: string }>;
+
+export async function generateMetadata(props: {
+  params: Params;
+}): Promise<Metadata> {
+  const params = await props.params;
+  const { id } = params;
+
+  if (!id) {
+    return {
+      title: "Pokemon Not Found",
+      description: "The requested Pokémon does not exist.",
+    };
+  }
+
+  const pokemon = await fetchPokemonById(id);
+  if (!pokemon) {
+    return {
+      title: "Pokemon Not Found",
+      description: "The requested Pokémon does not exist.",
+    };
+  }
+
+  return {
+    title: `Pokemon: ${pokemon.name}`,
+    description: `Details about ${pokemon.name}, including type, abilities, and stats.`,
+    openGraph: {
+      title: `Pokemon: ${pokemon.name}`,
+      description: `Explore ${pokemon.name}'s stats, abilities, and type.`,
+      images: [
+        {
+          url: pokemon.sprites.other["official-artwork"].front_default,
+          width: 400,
+          height: 400,
+          alt: `${pokemon.name} official artwork`,
+        },
+      ],
+    },
+  };
 }
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
+export default async function Page({ params }: { params: Params }) {
   const { id } = await params;
-  const pokemon = await fetchPokemonById(id);
 
+  const pokemon = await fetchPokemonById(id);
   if (!pokemon) return notFound();
 
   return <PokemonDetails pokemon={pokemon} />;
